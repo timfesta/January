ae761bde41a956b7902e1d1facbb0e77f642fc91
timfesta <timothyfesta@yahoo.com>
2016-02-03 05:56:30 -0800

    checkout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
 
 	def show
-		@user = User.find(params[:id])
-	end
+	    @user = User.find(params[:id])
+	  end
 
 	def new
 		@user = User.new
 	end
 
 	def create
-		user = User.new(user_params)
-		if user.save
-			session[:user_id] = user.id
-			redirect_to profile_path
-		else
-			redirect_to signup_path
-		end
-	end
+	    @user = User.new(user_params)
+	    if @user.save
+	    	flash[:success] = "Welcome to the Sample App!"
+	      redirect_to @user
+	    else
+	      render 'new'
+	    end
+	  end
 
 	private
-		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password)
-		end
+	def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,6 @@ ActiveRecord::Schema.define(version: 20160202135028) do
     t.string   "password_digest"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
 end
