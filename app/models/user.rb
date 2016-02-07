b813bf875c9dc7f3e580500d5f71fd19834aabc0
timfesta <timothyfesta@yahoo.com>
2016-02-07 11:28:49 -0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	                    format: { with: VALID_EMAIL_REGEX },
 	                    uniqueness: { case_sensitive: false }
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	
 	# Returns the hash digest of the given string.
 	 def User.digest(string)
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
 	   BCrypt::Password.create(string, cost: cost)
 	 end
 
+
+	 # Returns a random token.
 	 def User.new_token
 	    SecureRandom.urlsafe_base64
 	 end
