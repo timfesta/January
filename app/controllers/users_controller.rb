2156763a75e087cb1db7912efb5c625a39fa5451
timfesta <timothyfesta@yahoo.com>
2016-02-15 10:39:31 -0800

    root url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     @user.send_activation_email
+          UserMailer.account_activation(@user).deliver_now
           flash[:info] = "Please check your email to activate your account."
-          redirect_to root_url
+          redirect_to https://aqueous-dusk-8692.herokuapp.com/
     else
       render 'new'
     end
