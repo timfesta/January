8bf5c87b7a810b87815bbaa0a97d2d439c0c37a4
timfesta <timothyfesta@yahoo.com>
2016-02-15 20:36:15 -0800

    passwords

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => ENV['app45944488@heroku.com'],
+      :password       => ENV['fvjungvs2139'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
     }
