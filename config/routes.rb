1e3fbbcc8853b7650c9428799e14177fd13c89b0
timfesta <timothyfesta@yahoo.com>
2016-02-20 09:45:40 -0800

    reverted back to 6346152

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'users/new'
+
   	root             'static_pages#home'
     get 	'help'    => 'static_pages#help'
     get 	'about'   => 'static_pages#about'
@@ -11,5 +17,4 @@ Rails.application.routes.draw do
     resources :users
     resources :account_activations, only: [:edit]
     resources :password_resets,     only: [:new, :create, :edit, :update]
-    resources :microposts,          only: [:create, :destroy]
 end
