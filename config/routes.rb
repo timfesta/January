06773f7fe37529330df2c2857db13c40cfacfc51
timfesta <timothyfesta@yahoo.com>
2016-03-03 05:39:01 -0800

    added additional styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
     get 	'signup'  => 'users#new'
     get 	'login'   => 'sessions#new'
     post	'login'   => 'sessions#create'
-    delete  'logout'  => 'sessions#destroy'
+    get     'logout'  => 'sessions#destroy'
 
-    root             'static_pages#home'
-
-    resources :users do
-       member do
-         get :following, :followers
-       end
-     end
     resources :account_activations, only: [:edit]
     resources :password_resets,     only: [:new, :create, :edit, :update]
     resources :microposts,          only: [:create, :destroy]
     resources :relationships,       only: [:create, :destroy]
    
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
+
+    root             'static_pages#home'
 end
